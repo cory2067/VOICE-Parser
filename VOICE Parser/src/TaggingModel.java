431390c5ddf3846970756394ee6f7a2431ba07e5
Cory Lynch <cjl2625@gmail.com>
2015-09-14 16:28:34 -0400

    Cleared out old parser, set up sentence recognition

--- VOICE Parser/src/TaggingModel.java
+++ VOICE Parser/src/TaggingModel.java
@@ -1,20 +1,24 @@
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
 import opennlp.tools.cmdline.PerformanceMonitor;
 import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.WhitespaceTokenizer;
-import opennlp.tools.util.ObjectStream;
-import opennlp.tools.util.PlainTextByLineStream;
 
 public class TaggingModel {
 
-	public static Map<String, String> syntacticTagging(String input) throws IOException {
+	public static Map<String, String> POSTag(String input) {
 		Map<String, String> wordsTag = new HashMap<String, String>();
 		
 		//part of speech model
@@ -22,13 +26,13 @@ public class TaggingModel {
 		PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
 		POSTaggerME tagger = new POSTaggerME(model);
 
-		ObjectStream<String> lineStream = new PlainTextByLineStream(new StringReader(input));
+		Scanner reader = new Scanner(input);
+		//ObjectStream<String> lineStream = new PlainTextByLineStream(new StringReader(input));
 
 
 		perfMon.start();
-		String line;
-		while ((line = lineStream.read()) != null) {
-
+		while (reader.hasNext()) {
+			String line = reader.nextLine();
 			String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE.tokenize(line);
 			String[] tags = tagger.tag(whitespaceTokenizerLine);
 			//for tag reference, see: http://blog.dpdearing.com/2011/12/opennlp-part-of-speech-pos-tags-penn-english-treebank/
@@ -44,15 +48,33 @@ public class TaggingModel {
 
 			perfMon.incrementCounter();
 		}
+		reader.close();
 		perfMon.stopAndPrintFinalResult();
 		return wordsTag;
 	}
 	
 	
+	public static String[] getSentences(String text)
+	{
+		String sentences[] = {};
+		try {
+			InputStream modelIn = new FileInputStream("models/en-sent.bin");
+			SentenceModel model = new SentenceModel(modelIn);
+			SentenceDetectorME detector = new SentenceDetectorME(model);
+			sentences = detector.sentDetect(text);
+			
+			modelIn.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return sentences;
+	}
  
 	public static void main(String[] args) throws IOException {
 		String input = "write  for 1-5 min eppendorf max 4° C";
-		System.out.print(TaggingModel.syntacticTagging(input).keySet().toString());
+		System.out.print(TaggingModel.POSTag(input).keySet().toString());
 		
 	}
 }
--- VOICE Parser/src/VOICEParser.java
+++ VOICE Parser/src/VOICEParser.java
@@ -1,12 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
 
 /** 
  * @author Cory Lynch
  */
 public class VOICEParser 
 {
-
 	public static void main(String[] args)
 	{
-
+		/*Map<String, String> tagged = TaggingModel.POSTag("I like to eat sandwiches. They are my favorite food.");
+		for(String key : tagged.keySet())
+		{
+			System.out.println(key + ": " + tagged.get(key));
+		}*/
+		String[] s = TaggingModel.getSentences("2.3 million people suffer from Disease X. Mr. Scientist has discovered a new cure. The results of the study will be released on Nov. 25.");
+		for(String sent : s)
+		{
+			System.out.println("S: " + sent);
+		}
 	}
 }
