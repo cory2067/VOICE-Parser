f3429372f4eb7c8366af48100fb62cd0db3b1161
cory2067 <cjl2625@gmail.com>
2015-10-05 00:36:04 -0400

    New training model

--- VOICE Parser/src/VOICEParser.java
+++ VOICE Parser/src/VOICEParser.java
@@ -9,10 +9,23 @@ import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.util.Span;
 
-import java.io.File;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
 
 /** 
  * @author Cory Lynch
@@ -22,96 +35,158 @@ public class VOICEParser
 	private static final double keywordBiasFactor = 0.08;
 	
 	private static final String[] 
-			opportunity = {"$", "dollars", "million", "market"},
-			features = {"improved", "less toxic", "faster", "targeted therapy", "affected", "toxicity",
+			OPPORTUNITY = {"$", "dollars", "million", "market"},
+			FEATURES = {"improved", "less toxic", "faster", "targeted therapy", "affected", "toxicity",
 						"specificity", "more effective", "advantages"},
-			innovation = {"were shown to", "novel", "more natural", "we have discovered", "discovery",
+			INNOVATION = {"were shown to", "novel", "more natural", "we have discovered", "discovery",
 						"new therapeutic use", "great promise", "non-invasive", "innovative", "solution",
 						"minimize risk"},
-			challenge = {"penetrate blood brain barrier", "traditional", "lower toxicity", "higher specificity",
+			CHALLENGE = {"penetrate blood brain barrier", "traditional", "lower toxicity", "higher specificity",
 						"situation", "limited", "although", "however"},
-			application = {"targeted to", "application", "can be used", "potential products", 
+			APPLICATION = {"targeted to", "application", "can be used", "potential products", 
 						"differentiate", "treatment"};
+	
+	private static final String[] OUTPUT_LABELS = {"Market Opportunity", "Key Features",
+							"Our Innovation", "Scientific Challenge - Unmet Needs", "Projected Application"};
+												
+	private static TokenizerModel tmodel;
+	private static SentenceModel smodel;
+	private static DoccatModel dmodel;
+	private static TokenNameFinderModel[] nmodels = new TokenNameFinderModel[3];
+	
+	private static JTextArea input;
+	private static JFrame frame;
+	private static JTextArea out;
 			
 	public static void main(String[] args)
 	{
-		//process("Although peptides have shown some promise in clinical trials as therapeutic agents, their success has largely been limited by several factors, including rapid degradation by peptidases, poor cell permeability, and a lack of binding specificity resulting from conformational inflexibility. Science has overcome these limitations with the advance of peptidomimetics--a system for the production of modified chemical compounds capable of mimicking the structural and or functional properties of peptides. A fertile ground for such efforts lies in the discovery of ligands for nervous system receptors that mediate the sensation of pain. Researchers at Harvard University have developed a novel approach to generate nonpeptidic ligands to a key receptor involved in pain relief, the mu opioid receptor (MOR). This approach has far-reaching implications for the discovery of next-generation therapeutics for the treatment of pain. The peptidomimetic mu receptor ligands generated as a result of this synthetic platform are lead compounds for the treatment of pain. The market for pain therapeutics is very large. In one example, there are 45 million Americans that suffer from chronic headaches, while nearly 6 million reported case of chest pain. For more information, please contact Daniel Nadis.");
-		//process("Our company, Occams Resources, does cool stuff. For more information, please contact Daniel Nadis. Or, you can stop by the office, located at 59 Elm Street, New Haven, Connecticut.");
-		//process("Pierre Vinken has developed an innovative new approach to synthesizing RNA-623, a major breakthrough for the medical field. This method provides a more efficient solution than previous methods, and creates the substance quickly and effectively. 55 million Americans could benefit from this, and it has incredible promise. However, this method has certain limitations, as there is a maximum amount of substance that can be produced. Nevertheless, such a product has unlimited applications in the field of medicine.");
-		process("Technology Summary 6-2009-2241 | Liquid precursor composition that upon solidification forms a sustained release varnish Steinberg Doron, HUJI, Faculty of Dental Medicine, Oral Medicine Friedman Michael, HUJI, School of Pharmacy, Pharmaceutics Delivers treatment in response to pH conditions in mouth Categories Coatings, Biomaterials, Polymers, Composites, Dental Care Development Stage Successful clinical trials – ready for commercialization Patent Status Patent pending Market The oral drug delivery market, the largest segment of the drug delivery market, is a $35 billion industry expected to grow up to 10% per year Highlights Novel dental coating that delivers therapeutics over time, as required, in response to sensed changes in acidic/basic conditions in the oral cavity Dental disorders occur when there are changes in oral hygiene, diet, medicaments or age The main advantage of current therapeutic agents and drug delivery systems is their high substantivity in the target organ, in contrast to the applications currently used. Application-targeted reagent is suitable for oral disorders as: tooth decay, periodontal diseases, implantitis, halitosis, tooth whitening, hypersensitivity, cancer, fungal infection, repair and regeneration of bone, cartilage, tendon and ligament defects. Our Innovation The application is a pharmaceutical platform that various types of drugs can be incorporated into. Key Features Increases exposure time to medication, preventing infections even in procedures in the root canal Enables sustained release treatment effective against bacteria, yeasts, viruses and fungi Reduces amount of drugs required, minimizing side effects. These advantages result in better clinical improvement and better patient compliance. Built-in pH sensor increases the rate of release of medication in response to changes in pH that occur in many pathological oral disorders such as infections, caries, inflammations, dry mouth, halitosis, candidiasis, delivering medication as required Simple production process; straightforward scale-up Development Milestones Seeking industry cooperation for commercialization The Opportunity Additional applications in medicine in human or in animals http://www.yissum.co.il/sites/default/files/imagecache/category_icon_s/category_icons/import_id/93_1.png Contact for more information: http://www.yissum.co.il/sites/default/files/imagecache/project_resp_person/users/picture-1754.jpg Shoshana Keynan VP, Head of Business Development, Healthcare http://www.yissum.co.il/sites/default/themes/yissum/css/images/op_send.gif+972-2-6586683 All projects by: Friedman Michael (4) Steinberg Doron (7) Related projects (6) Yissum Research Development Company of the Hebrew University of Jerusalem Hi-Tech Park, Edmond J. Safra Campus, Givat-Ram, Jerusalem P.O. Box 39135, Jerusalem 91390 Israel Telephone: 972-2-658-6688, Fax: 972-2-658-6689");
+		//Although peptides have shown some promise in clinical trials as therapeutic agents, their success has largely been limited by several factors, including rapid degradation by peptidases, poor cell permeability, and a lack of binding specificity resulting from conformational inflexibility. Science has overcome these limitations with the advance of peptidomimetics--a system for the production of modified chemical compounds capable of mimicking the structural and or functional properties of peptides. A fertile ground for such efforts lies in the discovery of ligands for nervous system receptors that mediate the sensation of pain. Researchers at Harvard University have developed a novel approach to generate nonpeptidic ligands to a key receptor involved in pain relief, the mu opioid receptor (MOR). This approach has far-reaching implications for the discovery of next-generation therapeutics for the treatment of pain. The peptidomimetic mu receptor ligands generated as a result of this synthetic platform are lead compounds for the treatment of pain. The market for pain therapeutics is very large. In one example, there are 45 million Americans that suffer from chronic headaches, while nearly 6 million reported case of chest pain.
+		frame = new JFrame("VOICE Parser");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new FlowLayout());
+		JLabel msg = new JLabel("Initializing...");
+		msg.setFont(new Font("Arial", Font.BOLD, 24));
+		frame.add(msg);
+		frame.setSize(640, 700);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		
+		try {
+			init();
+		}
+		catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "Error: Could not initialize:\n" + e.getMessage());
+			System.exit(-1);
+		}
+		
+		input = new JTextArea("");
+		input.setRows(16);
+		input.setSize(600, 600);
+		input.setLineWrap(true);
+	    JScrollPane scroll = new JScrollPane(input);
+	    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		JButton submit = new JButton("Submit");
+		submit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				submit();
+			}
+		});
+		
+		out = new JTextArea("");
+		out.setRows(20);
+		out.setSize(600, 600);
+		out.setLineWrap(true);
+		out.setEditable(false);
+	    JScrollPane scrollOut = new JScrollPane(out);
+	    scrollOut.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		msg.setText("Enter a summary:");
+		frame.add(scroll);
+		frame.add(submit);
+		frame.add(scrollOut);
+		frame.setVisible(true);
 	}
 	
-	public static void process(String text)
+	public static void submit()
 	{
-		InputStream modelPerIn = null, modelTokIn = null;
-		try {
-			modelTokIn = new FileInputStream("models/en-token.bin");
-			TokenizerModel tmodel = new TokenizerModel(modelTokIn);
-			TokenNameFinderModel[] nmodels = {
-					new TokenNameFinderModel(new FileInputStream("models/en-ner-person.bin")),
-					new TokenNameFinderModel(new FileInputStream("models/en-ner-location.bin")),
-					new TokenNameFinderModel(new FileInputStream("models/en-ner-organization.bin"))};
-			NameFinderME nameFinder[] = new NameFinderME[3];
-			Tokenizer tokenizer = new TokenizerME(tmodel);
-			String[] tokens = tokenizer.tokenize(text);
-			final String[] label = {"Names" , "Locations", "Organizations"};
-			for(int a=0; a<3; a++)
+		out.setText(process(input.getText()));
+	    out.setCaretPosition(0);
+		frame.setVisible(true);
+	}
+	
+	public static void init() throws Exception
+	{
+		InputStream streams[] = {new FileInputStream("models/en-token.bin"), 
+								 new FileInputStream("models/en-sent.bin"),
+								 new FileInputStream("models/voice-classify.bin"),
+								 new FileInputStream("models/en-ner-person.bin"),
+								 new FileInputStream("models/en-ner-location.bin"),
+								 new FileInputStream("models/en-ner-organization.bin")};
+		
+		tmodel = new TokenizerModel(streams[0]);
+		smodel = new SentenceModel(streams[1]);
+		dmodel = new DoccatModel(streams[2]);
+		nmodels[0] = new TokenNameFinderModel(streams[3]);
+		nmodels[1] = new TokenNameFinderModel(streams[4]);
+		nmodels[2] = new TokenNameFinderModel(streams[5]);
+		
+		for(InputStream i : streams)
+			i.close();
+	}
+	
+	/* Output array:
+	 * 0: person
+	 * 1: location
+	 * 2: organization
+	 * 3: opportunity
+	 * 4: features
+	 * 5: innovation
+	 * 6: challenge
+	 * 7: application
+	 */
+	public static String process(String text)
+	{
+		StringBuilder output = new StringBuilder();
+		
+		NameFinderME nameFinder[] = new NameFinderME[3];
+		Tokenizer tokenizer = new TokenizerME(tmodel);
+		String[] tokens = tokenizer.tokenize(text);
+		final String[] label = {"Names" , "Locations", "Organizations"};
+		for(int a=0; a<3; a++)
+		{
+			nameFinder[a] = new NameFinderME(nmodels[a]);
+			Span nameSpans[] = nameFinder[a].find(tokens);
+			nameFinder[a].clearAdaptiveData();
+			
+			if(nameSpans.length > 0)
+				output.append(label[a] + " detected:\n");
+			else
+				continue;
+			
+			StringBuilder all = new StringBuilder();
+			for(Span s : nameSpans)
 			{
-				nameFinder[a] = new NameFinderME(nmodels[a]);
-				Span nameSpans[] = nameFinder[a].find(tokens);
-				nameFinder[a].clearAdaptiveData();
-				
-				System.out.println(label[a] + " detected:");
-				for(Span s : nameSpans)
-				{
-					String[] name = Arrays.copyOfRange(tokens, s.getStart(), s.getEnd());
-					StringBuilder builder = new StringBuilder();
-					for(String n : name) {
-					    builder.append(n + " ");
-					}
-					System.out.println(builder.toString().trim());
+				String[] name = Arrays.copyOfRange(tokens, s.getStart(), s.getEnd());
+				StringBuilder builder = new StringBuilder();
+				for(String n : name) {
+				    builder.append(n + " ");
 				}
+				System.out.println(builder.toString().trim());
+				all.append(builder.toString().trim() + "; ");
 			}
-		}
-		catch (Exception e) {
-		  e.printStackTrace();
-		}
-		finally {
-		  if (modelTokIn != null) {
-		    try {
-		      modelTokIn.close();
-		    }
-		    catch (Exception e) {
-		    }
-		  }
-		  if (modelPerIn != null) {
-			    try {
-			      modelPerIn.close();
-			    }
-			    catch (Exception e) {
-			    }
-		  }
+			if(all.length() != 0)
+				all.delete(all.length()-2, all.length()); 
+			output.append(all + "\n\n");
 		}
 
-		
+
 		String sentences[] = {};
 		DocumentCategorizerME classificationME = null;
-		try {
-			InputStream modelIn = new FileInputStream("models/en-sent.bin");
-			SentenceModel model = new SentenceModel(modelIn);
-			SentenceDetectorME detector = new SentenceDetectorME(model);
-			sentences = detector.sentDetect(text);
-			modelIn.close();
-			
-			 File test = new File("models/voice-classify.bin");
-			  String classificationModelFilePath = test.getAbsolutePath();
-				classificationME = new DocumentCategorizerME(
-				    new DoccatModel(
-				      new FileInputStream(classificationModelFilePath)));
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Error: could not initialize");
-		}
+		SentenceDetectorME detector = new SentenceDetectorME(smodel);
+		sentences = detector.sentDetect(text);
+		
+		classificationME = new DocumentCategorizerME(dmodel);
 		
 		Sentence[] sents = new Sentence[sentences.length];
 		int opIndex = classificationME.getIndex("opportunity");
@@ -125,17 +200,18 @@ public class VOICEParser
 			sents[a] = new Sentence(sentences[a],
 					classificationME.categorize(sentences[a]));
 			
-			sents[a].vals[opIndex] += bias(sentences[a], opportunity);
-			sents[a].vals[feIndex] += bias(sentences[a], features);
-			sents[a].vals[inIndex] += bias(sentences[a], innovation);
-			sents[a].vals[chIndex] += bias(sentences[a], challenge);
-			sents[a].vals[apIndex] += bias(sentences[a], application);
+			sents[a].vals[opIndex] += bias(sentences[a], OPPORTUNITY);
+			sents[a].vals[feIndex] += bias(sentences[a], FEATURES);
+			sents[a].vals[inIndex] += bias(sentences[a], INNOVATION);
+			sents[a].vals[chIndex] += bias(sentences[a], CHALLENGE);
+			sents[a].vals[apIndex] += bias(sentences[a], APPLICATION);
 		}
 		
 		String[] categories = {"opportunity", "features", "innovation", "challenge", "application"};
 		
-		for(String cat : categories)
+		for(int q=0; q<categories.length; q++)
 		{
+			String cat = categories[q];
 			double max = -1.0;
 			int best = -1;
 			for(int a = 0; a < sents.length; a++)
@@ -156,11 +232,13 @@ public class VOICEParser
 				continue;
 			}
 			
-			System.out.println("The choice for " + cat + ":");
-			System.out.println(sents[best].content);
-			System.out.println("Confidence: " + (int)(max*100) + "%\n");
+			output.append(OUTPUT_LABELS[q] + ":\n");
+			output.append(sents[best].content + "\n");
+			output.append("Confidence: " + (int)(max*100) + "%\n\n");
 			sents[best] = null;
 		}
+		
+		return output.toString();
 	}
 	
 	public static double bias(String s, String[] words)
